6aa99ddb03effda14f00e1e4551fb4ec1c1f48a0
pepeien <erickfcaldeira@gmail.com>
2024-01-15 21:59:45 -0300

    Initial commit

---
+++ Source/Main.cpp
@@ -0,0 +1,35 @@
+#include "Main.hpp"
+
+int main(int argc, char *argv[])
+{
+    try
+        {
+            std::unique_ptr<Engine::Actor> floor = std::make_unique<Engine::Actor>();
+            floor->setAbsoluteTranslation(glm::vec3(0.0f, 0.0f, 0.0f));
+            floor->setAbsoluteScale(glm::vec3(512.0f, 512.0f, 20.0f));
+            floor->setMesh({
+                "floor",
+                "grid"
+            });
+
+            std::unique_ptr<Engine::Level> level = std::make_unique<Engine::Level>();
+            level->addActor(floor.get());
+
+            Engine::WindowCreateInfo windowCreateInfo = {};
+            windowCreateInfo.title             = "Toast' Em";
+            windowCreateInfo.resolution.width  = 1600;
+            windowCreateInfo.resolution.height = 900;
+            windowCreateInfo.level             = level.get();
+
+            std::unique_ptr<Engine::Window> window = std::make_unique<Engine::Window>(windowCreateInfo);
+            window->run();
+        }
+        catch (const std::exception& e)
+        {
+            LOG_CRITICAL(e.what());
+
+            return EXIT_FAILURE;
+        }
+
+    return EXIT_SUCCESS;
+}
---
+++ Source/Main.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "Core.hpp"
+
+int main(int argc, char *argv[]);
