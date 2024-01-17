3d12da119323ec169f017c70334b88ce82a6e67d
pepeien <erickfcaldeira@gmail.com>
2024-01-17 18:38:59 -0300

    Implemented asset packing

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,33 +3,30 @@
 int main(int argc, char *argv[])
 {
     try
-        {
-            std::unique_ptr<Engine::Actor> floor = std::make_unique<Engine::Actor>();
-            floor->setAbsoluteTranslation(glm::vec3(0.0f, 0.0f, 0.0f));
-            floor->setAbsoluteScale(glm::vec3(512.0f, 512.0f, 20.0f));
-            floor->setMesh({
-                "floor",
-                "grid"
-            });
+    {
+        std::unique_ptr<Engine::Actor> floor = std::make_unique<Engine::Actor>();
+        floor->setAbsoluteTranslation(glm::vec3(0.0f, 0.0f, 0.0f));
+        floor->setAbsoluteScale(glm::vec3(0.1f, 0.1f, 0.1f));
+        floor->setMesh("./Content/Meshes/Air_Craft.pak");
 
-            std::unique_ptr<Engine::Level> level = std::make_unique<Engine::Level>();
-            level->addActor(floor.get());
+        std::unique_ptr<Engine::Level> level = std::make_unique<Engine::Level>();
+        level->addActor(floor.get());
 
-            Engine::WindowCreateInfo windowCreateInfo = {};
-            windowCreateInfo.title             = "Toast' Em";
-            windowCreateInfo.resolution.width  = 1600;
-            windowCreateInfo.resolution.height = 900;
-            windowCreateInfo.level             = level.get();
+        Engine::WindowCreateInfo windowCreateInfo = {};
+        windowCreateInfo.title             = "Toast' Em";
+        windowCreateInfo.resolution.width  = 1600;
+        windowCreateInfo.resolution.height = 900;
+        windowCreateInfo.level             = level.get();
 
-            std::unique_ptr<Engine::Window> window = std::make_unique<Engine::Window>(windowCreateInfo);
-            window->run();
-        }
-        catch (const std::exception& e)
-        {
-            LOG_CRITICAL(e.what());
+        std::unique_ptr<Engine::Window> window = std::make_unique<Engine::Window>(windowCreateInfo);
+        window->run();
+    }
+    catch (const std::exception& e)
+    {
+        LOG_CRITICAL(e.what());
 
-            return EXIT_FAILURE;
-        }
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
